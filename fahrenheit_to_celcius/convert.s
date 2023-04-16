0a40324c5697a27036859a7bca6013f442b1075a
Niklas <38914125+n0c1337@users.noreply.github.com>
2023-04-16 17:04:28 +0200

    Update convert.s

--- fahrenheit_to_celcius/convert.s
+++ fahrenheit_to_celcius/convert.s
@@ -1,21 +1,16 @@
 .text
-
 .global convert_f_to_c
 
 convert_f_to_c:
     movl    $0, %edx
-    movl    $0, %esi
-    movl    $0, %eax
 
     movl    %edi, %esi # Fahrenheit value from omglulw c programm
     sub     $32, %esi # substract by 32
     
     movl    $5, %eax
-    mul     %esi      # multiply with 5
-    movl    %eax, %esi
-
-    movl    $9, %eax
-    div     %esi      # divide by 9
-    movl    %eax, %esi
+    imul    %esi      # multiply with 5
+    
+    movl    $9, %esi
+    idiv    %esi
     
     ret
