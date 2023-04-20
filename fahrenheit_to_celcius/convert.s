740532e27a4379b0bcc12c1ef173d1b8db2d36f1
Niklas <38914125+n0c1337@users.noreply.github.com>
2023-04-20 13:31:01 +0200

    Floating points

--- fahrenheit_to_celcius/convert.s
+++ fahrenheit_to_celcius/convert.s
@@ -1,15 +1,18 @@
-global convert_f_to_c
+section .data
+    float1: dd 32.0
+    float2: dd 5.0
+    float3: dd 9.0
+
 section .text
+    global convert_f_to_c
     convert_f_to_c:
-        mov    rdx, 0
+        movss   xmm1, dword[float1] ; move 32.0 in the xmm1 register
+        subss   xmm0, xmm1          ; subtract xmm0 by 32
 
-        mov    rsi, rdi ; Fahrenheit value from omglulw c programm
-        sub    rsi, 32  ; substract by 32
-        
-        mov    rax, 5
-        imul   rsi      ; multiply with 5
+        movss   xmm1, dword[float2] ; move 5.0 in the xmm1 register
+        mulss   xmm0, xmm1          ; multiply xmm0 and xmm1 register
 
-        mov    rsi, 9
-        idiv   rsi      ; divide by 9
+        movss   xmm1, dword[float3] ; move 9.0 in the xmm1 register
+        divss   xmm0, xmm1          ; divide xmm0 / xmm1
         
         ret
